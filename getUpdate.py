aafeecb5db3b9c6d030c74a4c9d3d12c946e2054
maruthgoyal <maruthgoyal@gmail.com>
2016-05-14 20:56:09 +0530

    Added getUpdate.py

---
+++ getUpdate.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+
+
+def getupdate(url):
+
+    website = urllib2.urlopen(url)
+    soup = BeautifulSoup(website, "html.parser")
+
+    div = soup.find("div", id="reason")
+    children = div.findChildren()
+
+    for child in children:
+
+        if child.name == 'a':
+
+            return unicode(child.string)
+    return None
+
