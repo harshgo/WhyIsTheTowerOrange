cbd848b2f3b9556ee654de6544c8ce12a1403bfb
maruthgoyal <maruthgoyal@gmail.com>
2016-05-16 14:31:10 +0530

    Bug fixes for getUpdate2()

--- getUpdate.py
+++ getUpdate.py
@@ -2,35 +2,37 @@ from bs4 import BeautifulSoup
 import urllib2
 import re
 
-CURRENT_POST_NUMBER_FILE_PATH = ''
+CURRENT_POST_NUMBER_FILE_PATH = 'current_post_number.txt'
 
 def getTitle(n, s):
-    
-    div = soup.find("div", id=re.compile("post-[0-9]+:[0-9]+")
+
+    div = s.find("div", id=re.compile("post-[0-9]+"))
     current_post = int(div['id'].split('-')[1])
-    
+
     title = None
-    
+
     if current_post > n:
-        
+
         children = div.findChildren()
-        
+
         for child in children:
-            
+
             if child.name == 'h2' and 'post-title' in child['class']:
-                
+
                 grandchildren = child.findChildren()
-                
+
                 for grandchild in grandchildren:
-                    
+
                     if grandchild.name == 'a':
-                        
+
                         title = unicode(grandchild.string)
                         break
-                    
-    return title
+
+    return (title, current_post)
 
 def getUpdate(url):
+
+    ''' DEPRECATED. DO NOT USE.'''
 
     website = urllib2.urlopen(url)
     soup = BeautifulSoup(website, "html.parser")
@@ -43,17 +45,22 @@ def getUpdate(url):
         if child.name == 'a':
 
             return unicode(child.string)
-            
+
     return None
 
 def getUpdate2(url):
-    
+
     website = urllib2.urlopen(url)
     soup = BeautifulSoup(website, 'html.parser')
-    
+
     with open(CURRENT_POST_NUMBER_FILE_PATH) as f:
         n = int(f.readline())
-    
-    title = getTitle(n, soup)
-    
+
+    title, current_post_number = getTitle(n, soup)
+
+    if title:
+
+        with open(CURRENT_POST_NUMBER_FILE_PATH, 'w+') as f:
+            f.write(str(current_post_number))
+
     return title
